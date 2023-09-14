3722b53c2b93c4f06ec4aeaf5757ce1724e5535f
Sujit-Panigrahi5 <panigrahisujit588@gmail.com>
2023-09-14 23:54:38 +0530

    add some code

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+
+const looseweight=document.getElementById("looseweight")
+const maintainweight=document.getElementById("maintainweight")
+const gainweight=document.getElementById("gainweight")
+
+looseweight.style.animation="move 1 2s"
+maintainweight.style.animation="move1 1 2s"
+gainweight.style.animation="move 1 2s"
+// document.querySelector("div").style.backgroundImage="./assets/bg_food.webp"
